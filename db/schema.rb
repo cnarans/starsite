00ff7a5ee29327cef989607f8fe0608d0746260e
Colin Narans <cnarans@gmail.com>
2016-09-08 10:00:38 -0500

    switched to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160907142704) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stars", force: :cascade do |t|
     t.string   "name"
     t.float    "x"
